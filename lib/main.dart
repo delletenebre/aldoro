1d19b09e0be79a391a896277a32a315daa8c67ce
Sergei Nezhinskikh <s.nezhinskikh@infocom.kg>
2026-03-10 18:04:00 +0600

    feat: Integrate CocoaPods for macOS dependency management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
+import 'providers/storage_provider.dart';
+import 'resources/storage.dart';
 
 Future<void> main() async {
   /// ожидаем инициализации модуля взаимодействия с нативным кодом
   WidgetsFlutterBinding.ensureInitialized();
 
-  // /// инициализируем локальное хранилище
-  // final sharedStorage = await SharedPreferences.getInstance();
+  /// инициализируем локальное хранилище
+  final sharedStorage = await SharedPreferences.getInstance();
 
-  // /// инициализируем защищённое хранилище
-  // const secureStorage = FlutterSecureStorage();
+  /// инициализируем защищённое хранилище
+  const secureStorage = FlutterSecureStorage();
 
   runApp(
     ProviderScope(
       retry: null,
       overrides: [
-        // storageProvider.overrideWithValue(
-        //   Storage(sharedStorage: sharedStorage, secureStorage: secureStorage),
-        // ),
+        storageProvider.overrideWithValue(
+          Storage(sharedStorage: sharedStorage, secureStorage: secureStorage),
+        ),
       ],
       child: const App(),
     ),
